7e1775790c03eccf13d912e22b613f5c42db96d7
tvaira <tvaira@free.fr>
2023-09-03 19:55:13 +0200

    Ajoute utilisation de la classe Point

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <Arduino.h>
+#include "Point.h" // La classe Point
+
+Point p0;
+Point p1(1.0, 2.0);
+Point p2(4.0, 0.0);
 
 void setup()
 {
+    p2.setX(4.0);
+    p2.setX(0.);
+    float distance    = p1.calculeDistance(p2);
+    Point pointMilieu = p1.determinePointMilieu(p2);
 }
 
 void loop()
